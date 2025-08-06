00ed8918731db8e85e1e924c081b2b4b7e10d7dd
pouriam23 <pouriajafari23@gmail.com>
2025-08-06 14:16:33 +0330

    Added new test

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -4,16 +4,32 @@ import (
 	"bytes"
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
+	"html"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"snippetbox.alexedwards.net/internal/models/mocks"
 	"testing"
 	"time"
 )
+
+// Define a regular expression to capture the CSRF token value
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	// Try to find the CSRF token from the HTML body
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+	// Return the unescaped token string
+	return html.UnescapeString(string(matches[1]))
+}
 
 func newTestApplication(t *testing.T) *application {
 	templateCache, err := newTemplateCache()
@@ -79,4 +95,25 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	bytes.TrimSpace(body)
 
 	return res.StatusCode, res.Header, string(body)
+}
+
+// This method sends a POST request to the test server.
+// The last parameter is a `url.Values` object that holds the form data.
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the response body from the test server.
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytes.TrimSpace(body)
+
+	// Return status code, headers, and body.
+	return rs.StatusCode, rs.Header, string(body)
 }
